fdd8845651c12e42e38ae7110dff3c5a7030fade
thomasjungblut <thomas.jungblut@gmail.com>
2012-01-09 17:58:06 +0100

    mavenization and ignore of target, testcases don't currently execute

---
+++ src/main/java/de/hwr/treecision/DecisionTree.java
@@ -0,0 +1,9 @@
+package de.hwr.treecision;
+
+public final class DecisionTree {
+
+    public static final void main(String[] args) {
+	
+    }
+
+}
---
+++ src/test/java/de/hwr/treecision/DecisionTreeTest.java
@@ -0,0 +1,12 @@
+package de.hwr.treecision;
+
+import junit.framework.TestCase;
+
+public class DecisionTreeTest extends TestCase {
+
+    public void testDecisionTree() throws Exception {
+	assertTrue(1!=(1+25));
+	System.out.println("Hello Test!");
+    }
+
+}
