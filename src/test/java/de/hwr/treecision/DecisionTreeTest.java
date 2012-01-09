5d749f69c2cf077ecc12178a7af0b5661d1d5e8e
thomasjungblut <thomas.jungblut@gmail.com>
2012-01-09 18:57:54 +0100

    test commit hook

--- src/test/java/de/hwr/treecision/DecisionTreeTest.java
+++ src/test/java/de/hwr/treecision/DecisionTreeTest.java
@@ -6,7 +6,7 @@ public class DecisionTreeTest extends TestCase {
 
     public void testDecisionTree() throws Exception {
 	assertTrue(1!=(1+25));
-	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 2!");
+	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 3!");
     }
 
 }
