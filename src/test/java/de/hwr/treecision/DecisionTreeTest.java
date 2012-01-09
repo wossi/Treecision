8a94f12e49eab23dd2e133ca33fbd94a940273d4
thomasjungblut <thomas.jungblut@gmail.com>
2012-01-09 19:52:14 +0100

    CI commit test once again...

--- src/test/java/de/hwr/treecision/DecisionTreeTest.java
+++ src/test/java/de/hwr/treecision/DecisionTreeTest.java
@@ -6,11 +6,7 @@ public class DecisionTreeTest extends TestCase {
 
     public void testDecisionTree() throws Exception {
 	assertTrue(1!=(1+25));
-<<<<<<< HEAD
-	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 5!");
-=======
-	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 3!");
->>>>>>> 5d749f69c2cf077ecc12178a7af0b5661d1d5e8e
+	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 6!");
     }
 
 }
