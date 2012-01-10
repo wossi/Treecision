fb4629627c718ffe29144715ce7c74790ba5b454
Marvin Ritter <marvin.ritter@freenet.de>
2012-01-10 10:24:37 +0100

    Test

--- src/test/java/de/hwr/treecision/DecisionTreeTest.java
+++ src/test/java/de/hwr/treecision/DecisionTreeTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class DecisionTreeTest extends TestCase {
 
     public void testDecisionTree() throws Exception {
-	assertTrue(1!=(1+25));
+	assertTrue(26!=(25));
 	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 8! This should work with manual tests now!");
     }
 
