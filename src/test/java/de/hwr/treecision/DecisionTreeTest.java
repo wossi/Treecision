a7c57e3cc4941e0d949dcbd53f637dc69e73679a
thomasjungblut <thomas.jungblut@gmail.com>
2012-01-10 10:26:20 +0100

    format and push test via https

--- src/test/java/de/hwr/treecision/DecisionTreeTest.java
+++ src/test/java/de/hwr/treecision/DecisionTreeTest.java
@@ -4,9 +4,11 @@ import junit.framework.TestCase;
 
 public class DecisionTreeTest extends TestCase {
 
-    public void testDecisionTree() throws Exception {
-	assertTrue(1!=(1+25));
-	System.out.println("Hello Test! We are currently testing the commit build on CloudBees! Test 8! This should work with manual tests now!");
-    }
+	public void testDecisionTree() throws Exception {
+		assertTrue(1 != (1 + 25));
+		System.out
+				.println("Hello Test! We are currently testing the commit build on CloudBees! Test 8! This should work with manual tests now!");
+		System.out.println("just another output");
+	}
 
 }
